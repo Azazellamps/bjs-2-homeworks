0472d51c62e6a2560508524fee3a21d062223207
Azazel test <Azazellampss@gmail.com>
2023-07-25 12:02:15 +0300

    Первое задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,17 @@
-"use strict"
-function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
-}
+"use strict";
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+function solveEquation(a, b, c) {
+	let arr = [];
+	let d = (b ** 2) - (4 * a * c);
+	if (d < 0) {
+		arr.push();
+	} else if (d == 0) {
+		let oneRoot = -b / (2 * a);
+		arr.push(oneRoot);
+	} else if (d > 0) {
+		let firstSolution = (-b + Math.sqrt(d)) / (2 * a);
+		let secondSolution = (-b - Math.sqrt(d)) / (2 * a);
+		arr.push(firstSolution, secondSolution);
+	}
+	return arr;
 }
