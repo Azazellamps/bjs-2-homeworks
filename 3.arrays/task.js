2f31d4680f1ef7a7b9ff7710143ffd447a038bc1
Azazel test <Azazellampss@gmail.com>
2023-08-01 13:10:43 +0300

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,22 @@
 function compareArrays(arr1, arr2) {
-  
+	let result;
+
+	if (arr1.length != arr2.length) {
+		result = false;
+	} else {
+		result = arr1.every((element, i) => element === arr2[i]);
+	}
+
+	return result;
 }
 
+
 function getUsersNamesInAgeRange(users, gender) {
-  
+	let rightUsers = users.filter(user => user.gender === gender);
+	let getAge = rightUsers.map(user => user.age).reduce((acc, age) => acc + age, 0);
+	if (isNaN((getAge / rightUsers.length))) {
+		return 0;
+	}
+	return (getAge / rightUsers.length);
 }
+  
